1a39a1dcb1931e41b74bdefee95a362f5a13d2b8
jerry.li <16907478@qq.com>
2012-04-18 11:10:25 +0800

    Initial Commit

---
+++ BeautifulDaRen/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeautifulDaRen
+//
+//  Created by jerry.li on 4/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
