417a5dbbcf8d38f50d22995c9ea5e1c619cf61be
mykoma <23775517@qq.com>
2012-05-04 18:52:40 +0800

    add files

---
+++ BeautifulDaRen/Model/WaresItem.m
@@ -0,0 +1,17 @@
+//
+//  WaresItem.m
+//  BeautifulDaRen
+//
+//  Created by gang liu on 5/1/12.
+//  Copyright (c) 2012 myriad. All rights reserved.
+//
+
+#import "WaresItem.h"
+
+@implementation WaresItem
+
+@synthesize itemId;
+@synthesize itemTitle;
+@synthesize itemImageData;
+
+@end
