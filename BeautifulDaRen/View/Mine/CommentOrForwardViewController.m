d62202a7f6620d6f1d1b4cbc8b966717b51e9d12
mykoma <23775517@qq.com>
2012-05-25 15:00:35 +0800

    Update

--- BeautifulDaRen/View/Mine/CommentOrForwardViewController.m
+++ BeautifulDaRen/View/Mine/CommentOrForwardViewController.m
@@ -75,8 +75,9 @@
 #pragma mark UITableViewDelegate
 -(CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    NSInteger index = [indexPath row] % 2;
-    return  (index == 0) ? 130 : 200;
+//    NSInteger index = [indexPath row] % 2;
+//    return  (index == 0) ? 130 : 200;
+    return 130;
 }
 
 #pragma mark UITableViewDataSource
@@ -85,56 +86,56 @@
     static NSString * atMeViewCellIdentifier = @"AtMeViewCell";
     UITableViewCell * cell = nil;
     cell = [tableView dequeueReusableCellWithIdentifier:atMeViewCellIdentifier];
-    NSInteger index = [indexPath row] % 2;
+//    NSInteger index = [indexPath row] % 2;
     if(cell == nil)
     {
-        cell = [[[NSBundle mainBundle] loadNibNamed:atMeViewCellIdentifier owner:self options:nil] objectAtIndex:index];
+        cell = [[[NSBundle mainBundle] loadNibNamed:atMeViewCellIdentifier owner:self options:nil] objectAtIndex:0];
     }
     AtMeViewCell * atMeCell = (AtMeViewCell*)cell;
     atMeCell.friendNameLabel.text = @"仁和春天光华店";
     atMeCell.shopNameLabel.text = @"仁和春天";
     atMeCell.brandLabel.text = @"好莱坞明星";
-    if(index == 1)
-    {
-        atMeCell.weiboImageView.image = [ViewHelper getBubbleImageWithWidth:120 height:90];
-        UIImageView * originalWeiboAvatarImageView = [[[UIImageView alloc] initWithFrame:CGRectMake(15, 8, 25, 25)] autorelease];
-        originalWeiboAvatarImageView.image = [UIImage imageNamed:@"avatar_big"];
-        [atMeCell.weiboView addSubview:originalWeiboAvatarImageView];
-        
-        UILabel * originalWeiBoNameLabel = [[UILabel alloc] initWithFrame:CGRectMake(50, 12, 150, 25)];
-        originalWeiBoNameLabel.font = [UIFont systemFontOfSize:12];
-        originalWeiBoNameLabel.text = @"原始微博name";
-        originalWeiBoNameLabel.backgroundColor = [UIColor clearColor];
-        [atMeCell.weiboView addSubview:originalWeiBoNameLabel];
-        
-        UILabel * originalWeiBoTimeLineLabel = [[UILabel alloc] initWithFrame:CGRectMake(220, 12, 60, 25)];
-        originalWeiBoTimeLineLabel.font = [UIFont systemFontOfSize:12];
-        originalWeiBoTimeLineLabel.backgroundColor = [UIColor clearColor];
-        originalWeiBoTimeLineLabel.text = @"12天以前";
-        [atMeCell.weiboView addSubview:originalWeiBoTimeLineLabel];
-        
-        UIImageView * originalWeiboItemImageView = [[[UIImageView alloc] initWithFrame:CGRectMake(15, 40, 60, 60)] autorelease];
-        originalWeiboItemImageView.image = [UIImage imageNamed:@"avatar_big"];
-        [atMeCell.weiboView addSubview:originalWeiboItemImageView];
-        
-        UILabel * originalWeiBoCostLabel = [[UILabel alloc] initWithFrame:CGRectMake(40, 83, 33, 15)];
-        originalWeiBoCostLabel.font = [UIFont systemFontOfSize:12];
-        originalWeiboItemImageView.backgroundColor = [UIColor clearColor];
-        originalWeiBoCostLabel.text = @"￥120";
-        [atMeCell.weiboView addSubview:originalWeiBoCostLabel];
-        
-        UILabel * originalWeiBoShopLabel = [[UILabel alloc] initWithFrame:CGRectMake(80, 50, 200, 20)];
-        originalWeiBoShopLabel.font = [UIFont systemFontOfSize:12];
-        originalWeiBoShopLabel.backgroundColor = [UIColor clearColor];
-        originalWeiBoShopLabel.text = @"商场：万达广场";
-        [atMeCell.weiboView addSubview:originalWeiBoShopLabel];
-        
-        UILabel * originalWeiBoBrandLabel = [[UILabel alloc] initWithFrame:CGRectMake(80, 75, 200, 20)];
-        originalWeiBoBrandLabel.font = [UIFont systemFontOfSize:12];
-        originalWeiBoBrandLabel.backgroundColor = [UIColor clearColor];
-        originalWeiBoBrandLabel.text = @"品牌：Louis Vuitton";
-        [atMeCell.weiboView addSubview:originalWeiBoBrandLabel];
-    }
+//    if(index == 1)
+//    {
+//        atMeCell.weiboImageView.image = [ViewHelper getBubbleImageWithWidth:120 height:90];
+//        UIImageView * originalWeiboAvatarImageView = [[[UIImageView alloc] initWithFrame:CGRectMake(15, 8, 25, 25)] autorelease];
+//        originalWeiboAvatarImageView.image = [UIImage imageNamed:@"avatar_big"];
+//        [atMeCell.weiboView addSubview:originalWeiboAvatarImageView];
+//        
+//        UILabel * originalWeiBoNameLabel = [[UILabel alloc] initWithFrame:CGRectMake(50, 12, 150, 25)];
+//        originalWeiBoNameLabel.font = [UIFont systemFontOfSize:12];
+//        originalWeiBoNameLabel.text = @"原始微博name";
+//        originalWeiBoNameLabel.backgroundColor = [UIColor clearColor];
+//        [atMeCell.weiboView addSubview:originalWeiBoNameLabel];
+//        
+//        UILabel * originalWeiBoTimeLineLabel = [[UILabel alloc] initWithFrame:CGRectMake(220, 12, 60, 25)];
+//        originalWeiBoTimeLineLabel.font = [UIFont systemFontOfSize:12];
+//        originalWeiBoTimeLineLabel.backgroundColor = [UIColor clearColor];
+//        originalWeiBoTimeLineLabel.text = @"12天以前";
+//        [atMeCell.weiboView addSubview:originalWeiBoTimeLineLabel];
+//        
+//        UIImageView * originalWeiboItemImageView = [[[UIImageView alloc] initWithFrame:CGRectMake(15, 40, 60, 60)] autorelease];
+//        originalWeiboItemImageView.image = [UIImage imageNamed:@"avatar_big"];
+//        [atMeCell.weiboView addSubview:originalWeiboItemImageView];
+//        
+//        UILabel * originalWeiBoCostLabel = [[UILabel alloc] initWithFrame:CGRectMake(40, 83, 33, 15)];
+//        originalWeiBoCostLabel.font = [UIFont systemFontOfSize:12];
+//        originalWeiboItemImageView.backgroundColor = [UIColor clearColor];
+//        originalWeiBoCostLabel.text = @"￥120";
+//        [atMeCell.weiboView addSubview:originalWeiBoCostLabel];
+//        
+//        UILabel * originalWeiBoShopLabel = [[UILabel alloc] initWithFrame:CGRectMake(80, 50, 200, 20)];
+//        originalWeiBoShopLabel.font = [UIFont systemFontOfSize:12];
+//        originalWeiBoShopLabel.backgroundColor = [UIColor clearColor];
+//        originalWeiBoShopLabel.text = @"商场：万达广场";
+//        [atMeCell.weiboView addSubview:originalWeiBoShopLabel];
+//        
+//        UILabel * originalWeiBoBrandLabel = [[UILabel alloc] initWithFrame:CGRectMake(80, 75, 200, 20)];
+//        originalWeiBoBrandLabel.font = [UIFont systemFontOfSize:12];
+//        originalWeiBoBrandLabel.backgroundColor = [UIColor clearColor];
+//        originalWeiBoBrandLabel.text = @"品牌：Louis Vuitton";
+//        [atMeCell.weiboView addSubview:originalWeiBoBrandLabel];
+//    }
     return cell;
 }
 
