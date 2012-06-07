cfe1e1d0cd253fc4c14f162066cb71a9f6c0f091
jerry.li <16907478@qq.com>
2012-06-07 18:07:18 +0800

    Fixed the notification error of login and register using sina weibo

--- BeautifulDaRen/View/Home/LoginViewController.m
+++ BeautifulDaRen/View/Home/LoginViewController.m
@@ -271,14 +271,13 @@
 {
     if(button ==  self.loginWithSinaWeiboButton)
     {
-        [[iToast makeText:@"新浪微博登陆"] show];
         if (![[SinaSDKManager sharedManager] isLogin])
         {
             [[SinaSDKManager sharedManager] setRootviewController:self.navigationController];
             [[SinaSDKManager sharedManager] loginWithDoneCallback:^(LOGIN_STATUS status) {
                 NSLog(@"Sina SDK login done, status:%d", status);
                 if (status == LOGIN_STATUS_SUCCESS) {
-                    [[iToast makeText:@"亲，认证成功过了！"] show];
+                    [[iToast makeText:@"亲，认证成功了！"] show];
                 }
                 else
                 {
